8cd1d8ad6281428039df6f6c66659617504ed5d1
Niko Strijbol <strijbol.niko@gmail.com>
2020-12-07 10:35:36 +0100

    Day 7

---
+++ 2020/src/day7.exs
@@ -0,0 +1,61 @@
+defmodule Day7 do
+  @behaviour Runner.Day
+  
+  def read(file) do
+    File.stream!(file)
+    |> Map.new(&parse_line/1)
+  end
+  
+  defp parse_colour(colour) do
+    [colour, _] = Regex.run(~r/(.*) (bag|bags)/, colour, capture: :all_but_first)
+    colour
+  end
+  
+  defp parse_line(line) do
+    [key, colours] = String.trim(line) |> String.split(" contain ")
+    key = parse_colour(key)
+    if colours == "no other bags." do
+      {key, []}
+    else
+      colours = String.split(colours, ", ")
+                |> Enum.map(fn colour ->
+        [amount, colour] = Regex.run(~r/^(\d+) (.*)$/, colour, capture: :all_but_first)
+        %{amount: String.to_integer(amount), colour: parse_colour(colour)}
+      end)
+      {key, colours}
+    end
+    
+  end
+  
+  defp contain?(map, color, search) do
+    containing = Map.get(map, color) |> Enum.map(fn %{colour: colour} -> colour end)
+    if search in containing do
+      true
+    else
+      Enum.any?(containing, fn c -> contain?(map, c, search) end)
+    end
+  end
+  
+  defp do_count(map, color) do
+    containing = Map.get(map, color)
+    c = Enum.map(containing, fn %{colour: c, amount: a} -> a * do_count(map, c) end)
+        |> Enum.sum()
+    c + 1
+  end
+  
+  defp count_amount(map, color), do: do_count(map, color) - 1
+  
+  @impl true
+  def part1(file) do
+    map = read(file)
+    Enum.count(map, fn {k, _} -> contain?(map, k, "shiny gold") end)
+    |> IO.inspect()
+  end
+
+  @impl true
+  def part2(file) do
+    read(file)
+    |> count_amount("shiny gold")
+    |> IO.inspect()
+  end
+end
