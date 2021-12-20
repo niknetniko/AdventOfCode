1e139be59b79dc99b478fe1e4de39d6673fdd794
Niko Strijbol <strijbol.niko@gmail.com>
2021-12-21 00:06:25 +0100

    Complete day 3

--- 2021/src/day03.ex
+++ 2021/src/day03.ex
@@ -1,61 +1,61 @@
 defmodule Day03 do
   @behaviour Day
 
-  use Bitwise
-
-  defp parse_number(str) do
-    String.codepoints(str)
-    |> Enum.map(&String.to_integer/1)
-  end
-
-  # From https://elixirforum.com/t/how-to-manipurate-bitstring-not-binary-such-as-making-a-value-concatinating-converting-into-integer/22654/4
-  defp pad_leading_zeros(bs) when is_binary(bs), do: bs
-  defp pad_leading_zeros(bs) when is_bitstring(bs) do
-    pad_length = 8 - rem(bit_size(bs), 8)
-    <<0::size(pad_length), bs::bitstring>>
-  end
-
-  defp find_most_common(data, position) do
-    data
-    |> Enum.reduce([0, 0], fn data, [z, o] -> [z + bxor(1, Enum.at(data, position)), o + Enum.at(data, position)] end)
-    |> Enum.with_index()
-    |> Enum.max_by(fn {v, _} -> v end)
-    |> then(fn {_, i} -> i end)
-  end
-
-  defp find_least_common(data, position) do
-    data
-    |> Enum.reduce([0, 0], fn data, [z, o] -> [z + bxor(1, Enum.at(data, position)), o + Enum.at(data, position)] end)
-    |> Enum.with_index()
-    |> Enum.min_by(fn {v, _} -> v end)
-    |> then(fn {_, i} -> i end)
-  end
-
   @impl true
   def part1(file) do
-    number = File.read!(file)
-    |> String.split("\n", trim: true)
-    |> Enum.map(&parse_number/1)
-    length = length(Enum.at(number, 0)) - 1
+    numbers =
+      File.read!(file)
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_charlist/1)
 
-    gamma = Enum.map(0..length, fn pos -> find_most_common(number, pos) end)
-            |> Enum.into(<<>>, fn b -> <<b:: 1>> end)
-            |> pad_leading_zeros()
-            |> :binary.decode_unsigned()
-    epsilon = Enum.map(0..length, fn pos -> find_least_common(number, pos) end)
-              |> Enum.into(<<>>, fn b -> <<b:: 1>> end)
-              |> pad_leading_zeros()
-              |> :binary.decode_unsigned()
+    # Get the length of the numbers
+    [first | _] = numbers
+    half_length = div(length(numbers), 2)
+
+    most_common =
+      Enum.map(0..(length(first) - 1), fn pos ->
+        zero_count = Enum.count_until(numbers, &(Enum.at(&1, pos) == ?0), half_length + 1)
+        if zero_count < half_length, do: ?1, else: ?0
+      end)
+
+    gamma = List.to_integer(most_common, 2)
+
+    epsilon =
+      Enum.map(most_common, fn bit -> if bit == ?1, do: ?0, else: ?1 end)
+      |> List.to_integer(2)
 
     gamma * epsilon
   end
 
-  def apply_command2(["forward", x], [h, d, a]), do: [h + x, d + x * a, a]
-  def apply_command2(["up", x], [h, d, a]), do: [h, d, a - x]
-  def apply_command2(["down", x], [h, d, a]), do: [h, d, a + x]
-
   @impl true
   def part2(file) do
+    numbers =
+      File.read!(file)
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_charlist/1)
 
+    [first | _] = numbers
+
+    [raw_oxygen] =
+      Enum.reduce_while(0..(length(first) - 1), numbers, fn pos, remaining_numbers ->
+        half = div(length(remaining_numbers), 2)
+        zero_count = Enum.count_until(remaining_numbers, &(Enum.at(&1, pos) == ?0), half + 1)
+        most_common_bit = if zero_count > half, do: ?0, else: ?1
+        remainder = Enum.filter(remaining_numbers, &(Enum.at(&1, pos) == most_common_bit))
+
+        if length(remainder) == 1, do: {:halt, remainder}, else: {:cont, remainder}
+      end)
+
+    [raw_scrubber] =
+      Enum.reduce_while(0..(length(first) - 1), numbers, fn pos, remaining_numbers ->
+        half = div(length(remaining_numbers), 2)
+        zero_count = Enum.count_until(remaining_numbers, &(Enum.at(&1, pos) == ?0), half + 1)
+        least_common_bit = if zero_count <= div(length(remaining_numbers), 2), do: ?0, else: ?1
+        remainder = Enum.filter(remaining_numbers, &(Enum.at(&1, pos) == least_common_bit))
+
+        if length(remainder) == 1, do: {:halt, remainder}, else: {:cont, remainder}
+      end)
+
+    List.to_integer(raw_oxygen, 2) * List.to_integer(raw_scrubber, 2)
   end
 end
